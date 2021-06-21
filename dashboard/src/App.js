538832ba453dd9f77faff285d5a240ab9e61e221
Lucas Desouza <lucasfds2777@gmail.com>
2021-06-21 17:59:01 -0500

    adding router

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,88 +1,25 @@
-import './App.css';
-import {useEffect, useState} from 'react';
-import {LinearProgress, Grid, Typography, Container, makeStyles} from "@material-ui/core";
-import {Alert} from "@material-ui/lab";
-import {ClimateControlNodeComponent, NodeComponent} from "./components/NodeComponent";
-import LoopIcon from '@material-ui/icons/Loop';
-import GetAppIcon from '@material-ui/icons/GetApp';
-import PowerSettingsNewIcon from '@material-ui/icons/PowerSettingsNew';
-import axios from 'axios';
-import Button from "@material-ui/core/Button";
-import ButtonGroup from "@material-ui/core/ButtonGroup";
-import {config} from "./config/";
-
-const useStyles = makeStyles({
-    root: {
-        marginTop: '3rem',
-        color: '#ecf0f1'
-    },
-    globalButton: {
-        margin: '.5rem'
-    },
-    section: {
-        marginTop: '3rem',
-    }
-});
-
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+import {Home} from "./components/Home";
 
 function App() {
-    const classes = useStyles();
-    const [nodes, setNodes] = useState([]);
-    const [error, setError] = useState("");
-    const [loading, setLoading] = useState(true);
-    const [reload, setReload] = useState(new Date());
-    const rebootAll = () => {
-        axios.get(`${config.baseUrl}/reboot-nodes`).catch(error => {
-            console.error(error)
-        })
-    };
-    const updateAll = () => {
-        axios.get(`${config.baseUrl}/update-nodes`).catch(error => {
-            console.error(error)
-        })
-    };
-    useEffect(() => {
-        setInterval(() => setReload(new Date()), 5 * 1000);
-    }, []);
-    useEffect(() => {
-        axios.get(`${config.baseUrl}/climate`).then(response => {
-            setNodes(response.data);
-        }).catch(error => {
-            setError("Something went wrong");
-        }).finally(() => {
-            setLoading(false);
-        })
-    }, [reload])
-    const triggerReload = (showLoading) => {
-        if (showLoading) setLoading(true);
-        setReload(new Date());
-    };
-    if (error !== "") return <Alert severity={'error'}>{error}</Alert>
-    if (loading) return <LinearProgress/>
-    return (
-        <Container className={classes.root}>
-            <Typography variant={'h1'}>PiNet</Typography>
-            <div className={classes.section}>
-                <Button variant={'contained'} size={'large'} className={classes.globalButton}
-                        onClick={triggerReload}><LoopIcon/> &nbsp; Force reload </Button>
-                <Button variant={'contained'} size={'large'} className={classes.globalButton}
-                        onClick={rebootAll}><PowerSettingsNewIcon/> &nbsp; Reboot all nodes </Button>
-                <Button variant={'contained'} size={'large'} className={classes.globalButton}
-                        onClick={updateAll}><GetAppIcon/> &nbsp; Update all nodes </Button>
-            </div>
-            <div className={classes.section}>
-                <Typography variant={'h2'}>Climate control nodes</Typography>
-                <Grid container>
-                    {nodes.map(node => <Grid item lg={4} md={6} xs={12}><ClimateControlNodeComponent
-                        reload={triggerReload} node={node}/></Grid>)}
-                </Grid>
-            </div>
-            <div className={classes.section}>
-                <Typography variant={'h2'}>Hub nodes</Typography>
-                <NodeComponent node={{node: "PiNet Hub", location: "office"}}/>
-            </div>
-        </Container>
-    );
+    return (<div>
+        <Router>
+            <Switch>
+                <Route path={"/"}>
+                    <Home/>
+                </Route>
+                <Route path={"*"}>
+                    <Home/>
+                </Route>
+            </Switch>
+        </Router>
+    </div>)
 }
 
 export default App;
