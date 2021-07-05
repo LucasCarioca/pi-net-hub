8968fd1d179220e7a837c67229f46b6cb0f98d72
Lucas Desouza <lucasfds2777@gmail.com>
2021-07-05 15:05:27 -0500

    fix url to api

--- dashboard/src/config/config.js
+++ dashboard/src/config/config.js
@@ -1,3 +1,3 @@
 export default {
-    baseUrl: process.env.NODE_ENV === "production" ? "/api"  : "http://localhost:8000/api"
+    baseUrl: process.env.NODE_ENV === "production" ? "https://pinet.ldkube.io/api"  : "http://localhost:8000/api"
 }
